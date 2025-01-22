43a2fab6cb47b23397fd12e55a93dc43747fa9f9
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2025-01-23 01:52:40 +0530

    add checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
-function App() {
 
+function App() {
   return (
     <MainLayout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/checkout" element={<CheckoutPage onCheckout={handleCheckout} />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </MainLayout>
